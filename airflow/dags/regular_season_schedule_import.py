d77c9baa9ef61177e0bf89109b42f919a652fea4
SwolenOstrich430 <60279013+SwolenOstrich430@users.noreply.github.com>
2025-11-19 16:27:42 -0600

    adding for now

---
+++ airflow/dags/regular_season_schedule_import.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonVirtualenvOperator
+from bball_reference_client.bball_reference_client import BballReferenceClient
+
+dag = DAG(
+    dag_id="regular_season_import",
+    schedule_interval="* * * 10 10 *",
+    start_date=datetime(2000, 1, 1),
+    catchup=True,
+)
+
+
+def _download_schedule(**kwargs):
+    client = BballReferenceClient()
+    logical_date = kwargs['logical_date']
+    year = logical_date.year + 1
+
+    raw_schedule = client.get_season_schedule_raw(year)
+    print(raw_schedule)
+
+
+download_regular_season_schedule = PythonVirtualenvOperator(
+    task_id="download_regular_season_schedule", 
+    python_callable=_download_schedule, 
+    dag=dag
+)
+
+download_regular_season_schedule >> download_regular_season_schedule >> exit
